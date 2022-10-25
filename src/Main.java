9f82396698fe92824769bc4f0b5cf3921caa645d
Alemania195 <jbarraganramirez0@gmail.com>
2022-10-25 16:43:50 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+
+
+
+
+                public class Main {
+                    public static void main(String[] args) {
+
+                        Scanner Sc = new Scanner(System.in);
+                        String NM;
+                        int dias;
+                        int horas1;
+                        int sueldo;
+                        String NM2;
+                        int dias2;
+                        int horas2;
+                        int sueldo2 = 0;
+                        String NM3;
+                        int dias3;
+                        int horas3;
+                        int sueldo3 = 0;
+                        String NM4;
+                        int dias4;
+                        int horas4;
+                        int sueldo4 = 0;
+                        String NM5;
+                        int dias5;
+                        int horas5;
+                        int sueldo5 = 0;
+
+
+                        sueldo = 700000;
+                        double horas;
+                        System.out.println("hola muy buenas tardes me puede regalar su Nombre XD");
+                        NM = Sc.nextLine();
+                        System.out.println(" hola muy buenas tardes me regala su edad");
+                        dias = Sc.nextInt();
+                        System.out.println(" hola muy buenas tardes digita tu horas trabajdas");
+                        horas = Sc.nextInt();
+                        System.out.println(" su nombre es" + NM + " ahora su edad es" + dias + " y sus horas son " + horas + "");
+                        System.out.println(" y su sueldo es" + sueldo + " gracias por consultar  el siguiente");
+
+                        System.out.println("hola muy buenas tardes me puede regalar su Nombre XD");
+                        NM2 = Sc.nextLine();
+                        System.out.println(" hola muy buenas tardes me regala su edad");
+                        dias2 = Sc.nextInt();
+                        System.out.println(" hola muy buenas tardes digita tu horas trabajdas");
+                        horas2 = Sc.nextInt();
+                        System.out.println(" su nombre es" + NM2 + " ahora su edad es" + dias2 + " y sus horas son " + horas2 + "");
+                        System.out.println(" y su sueldo es" + sueldo2 + " gracias por consultar  el siguiente");
+
+                        System.out.println("hola muy buenas tardes me puede regalar su Nombre XD");
+                        NM3 = Sc.nextLine();
+                        System.out.println(" hola muy buenas tardes me regala su edad");
+                        dias3 = Sc.nextInt();
+                        System.out.println(" hola muy buenas tardes digita tu horas trabajdas");
+                        horas3 = Sc.nextInt();
+                        System.out.println(" su nombre es" + NM3 + " ahora su edad es" + dias3 + " y sus horas son " + horas3 + "");
+                        System.out.println(" y su sueldo es" + sueldo3 + " gracias por consultar  el siguiente");
+
+                        System.out.println("hola muy buenas tardes me puede regalar su Nombre XD");
+                        NM4 = Sc.nextLine();
+                        System.out.println(" hola muy buenas tardes me regala su edad");
+                        dias4 = Sc.nextInt();
+                        System.out.println(" hola muy buenas tardes digita tu horas trabajdas");
+                        horas4 = Sc.nextInt();
+                        System.out.println(" su nombre es"+NM4+" ahora su edad es" +dias4+ " y sus horas son " +horas4+ "");
+                        System.out.println(" y su sueldo es" +sueldo4+ " gracias por consultar  el siguiente");
+
+                        System.out.println("hola muy buenas tardes me puede regalar su Nombre XD");
+                        NM5 = Sc.nextLine();
+                        System.out.println(" hola muy buenas tardes me regala su edad");
+                        dias5 = Sc.nextInt();
+                        System.out.println(" hola muy buenas tardes digita tu horas trabajdas");
+                        horas5 = Sc.nextInt();
+                        System.out.println(" su nombre es"+NM5+" ahora su edad es" +dias5+ " y sus horas son " +horas5+ "");
+                        System.out.println(" y su sueldo es" +sueldo5+" gracias por consultar  el siguiente");
+
+
+
+                    }
+                }
+            }
+                    }
+                    }
